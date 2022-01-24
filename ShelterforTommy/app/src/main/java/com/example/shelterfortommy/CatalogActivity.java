fe6b982d292ceaa37a80dcadf5716f2c4e2b5241
devayush20 <as.ayush20apr@gmail.com>
2022-01-25 01:27:14 +0530

    Creating, Reading and Writing to Database in Android
    Operations for various views and data fields are set.

---
+++ ShelterforTommy/app/src/main/java/com/example/shelterfortommy/CatalogActivity.java
@@ -0,0 +1,220 @@
+package com.example.shelterfortommy;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.shelterfortommy.data.PetDbHelper;
+import com.example.shelterfortommy.data.PetContract.PetEntry;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+/**
+ * Displays list of pets that were entered and stored in the app.
+ */
+public class CatalogActivity extends AppCompatActivity {
+
+    /**Database helper that will provide us access to the database */
+    private PetDbHelper mDbHelper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalog);
+        Log.i("###","entered onCreate()  in CatalogActivity");
+
+        //Set FAB to open EditorActivity
+        FloatingActionButton fab=(FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Log.i("###","entered onClick() inside Listener for FAB in CatalogActivity");
+
+                Intent intent=new Intent(CatalogActivity.this,EditorActivity.class);
+                startActivity(intent);
+                Log.i("###","activity editor started exiting onClick Listener for FAB in CatalogActivity");
+
+            }
+        });
+        //Get the instance of the PetDbHelper class
+        mDbHelper=new PetDbHelper(this);
+        Log.i("###","Obtained the instance of PetDbHelper");
+
+        Log.i("###","calling displayDatabaseInfo() inside Catalog activity");
+
+        displayDatabaseInfo();//to check if database is implemented correctly
+
+        Log.i("###","exiting onCreate() inside Catalog activity");
+        //For pulling the database file from device
+        //PetDbHelper mDbHelper = new PetDbHelper(this);
+
+    }
+
+    /**
+     * Following method will be invoked when the method
+     * finish() is called in the Editor Activity.
+     */
+
+    @Override
+    protected void onStart() {
+        Log.i("###","Entered the onStart() method");
+        super.onStart();
+        displayDatabaseInfo();
+        Log.i("###","exiting the onStart() method of Catalog activity");
+    }
+
+    /**
+     * Temporary helper method to display information in the onscreen TextView
+     * abou the state of the pets datbase
+     */
+    private void displayDatabaseInfo()
+    {
+        Log.i("###","entered displayDatabaseInfo() inside Catalog activity");
+
+        //To access our database, we instantiate our subclass of SQLiteOpenHelper
+        //and pass the context, which is the current activity.
+        //PetDbHelper mDbHelper=new PetDbHelper(this);
+        //Create and/or open a database to read from it.
+        SQLiteDatabase db=mDbHelper.getReadableDatabase();
+        Log.i("###","Database object obtained after creating/opening inside displayDatabaseInfo()");
+
+        //Perform this raw SQL query "SELCT * FROM pets"
+        //to get a Cursor that contains all rows from the pets table.
+        //Cursor cursor=db.rawQuery("SELECT * FROM "+PetEntry.TABLE_NAME,null);
+
+        //Concrete Implementation-define the projection array
+        //and obtain a cursor object
+        String []projection={PetEntry._ID,
+                PetEntry.COLUMN_PET_NAME,
+                PetEntry.COLUMN_PET_BREED,
+                PetEntry.COLUMN_PET_GENDER,
+                PetEntry.COLUMN_PET_WEIGHT};
+        Cursor cursor=db.query(PetEntry.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null);
+        Log.i("###","Cursor object obtained in displayDatabaseInfo()");
+
+        try {
+            //Display the number of rows in the cursor (which reflects the number
+            //of rows in the pets table in the database).
+            TextView displayView=(TextView) findViewById(R.id.text_view_pet);
+           // displayView.setText(getString(R.string.number_of_rows_in_pets_table_message)+cursor.getCount());
+
+            /*
+            Create a header in the Text that looks like this-
+            The pets table contains the <number of rows in Cursor> pets.
+            _id-name-breed-gender-weight
+
+            In the while loop, iterate through the rows of the cursor and display
+            the information from each column in this order.
+             */
+            displayView.setText("The pets table contains "+cursor.getCount()+" pets.\n\n");
+            displayView.append(PetEntry._ID + " - " +
+                    PetEntry.COLUMN_PET_NAME + " - " +
+                    PetEntry.COLUMN_PET_BREED + " - " +
+                    PetEntry.COLUMN_PET_GENDER + " - " +
+                    PetEntry.COLUMN_PET_WEIGHT + "\n");
+            //Figure out the index of each column.
+            int idColumnIndex = cursor.getColumnIndex(PetEntry._ID);
+            int nameColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_NAME);
+            int breedColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_BREED);
+            int genderColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_GENDER);
+            int weightColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_WEIGHT);
+            //Iterate through all the returned rows in the Cursor.
+            while(cursor.moveToNext())
+            {
+                // Use that index to extract the String or Int value of the word
+                // at the current row the cursor is on.
+                int currentID=cursor.getInt(idColumnIndex);
+                String currentName=cursor.getString(nameColumnIndex);
+                String currentBreed=cursor.getString(breedColumnIndex);
+                int currentGender=cursor.getInt(genderColumnIndex);
+                int currentWeight=cursor.getInt(weightColumnIndex);
+
+                //Display the values from each column of the current row in the
+                //cursor in the TextView
+                displayView.append("\n"+currentID+"-"+currentName+"-"+currentBreed+"-"+currentGender+"-"+currentWeight);
+            }
+            Log.i("###","no. of rows displayed in catalog activity.");
+        }
+        finally
+        {
+            //Always close the cursor when you're done reading from it.
+            //This releases all the resources makes it invalid.
+            cursor.close();
+            Log.i("###","Cursor closed!");
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //inflate the menu options from the res/menu./menu_catalog.xml file.
+        //This adds menu items to the app bar.
+        Log.i("###","entered onCreateOptionsMenu  in CatalogActivity");
+
+        getMenuInflater().inflate(R.menu.menu_catalog,menu);
+        Log.i("###","exiting onCreateOptionsMenu  in CatalogActivity" +
+                "after inflating menu catalog");
+
+        return true;
+    }
+    private void insertPet()
+    {
+        Log.i("###","Entered insertPet() method in CatalogActivity class");
+        //Get the database write mode
+        SQLiteDatabase db=mDbHelper.getWritableDatabase();
+        /*Create a ContentValues object where column names are the keys,
+        and pet's attributes are the values.
+         */
+        ContentValues values=new ContentValues();
+        values.put(PetEntry.COLUMN_PET_NAME,"Toto");
+        values.put(PetEntry.COLUMN_PET_BREED,"Terrier");
+        values.put(PetEntry.COLUMN_PET_GENDER, PetEntry.GENDER_MALE);
+        values.put(PetEntry.COLUMN_PET_WEIGHT,7);
+
+        // Insert a new row for Toto in the database, returning the ID of that new row.
+        // The first argument for db.insert() is the pets table name.
+        // The second argument provides the name of a column in which the framework
+        // can insert NULL in the event that the ContentValues is empty (if
+        // this is set to "null", then the framework will not insert a row when
+        // there are no values).
+        // The third argument is the ContentValues object containing the info for Toto.
+        long newRowId=db.insert(PetEntry.TABLE_NAME,null,values);
+        Log.i("###","invoked insert() method in insertPet() method in CatalogActivity class, now exiting");
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //When user clicks on a menu option in the app bar overflow menu
+        Log.i("###","entered onOptionsItemSelected() in CatalogActivity");
+        switch(item.getItemId())
+        {
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.action_insert_dummy_data:
+                //We will take some action when user clicks insert dummy data menu option
+                insertPet();
+                displayDatabaseInfo();//for testing purpose
+                return true;
+                //Respond to a click on the "Delete all entries" menu option
+            case R.id.action_delete_all_entries:
+                //Do nothing
+                return true;
+        }
+        Log.i("###","exiting onOptionsItemSelected() in CatalogActivity");
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ ShelterforTommy/app/src/main/java/com/example/shelterfortommy/EditorActivity.java
@@ -0,0 +1,186 @@
+package com.example.shelterfortommy;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
+
+import com.example.shelterfortommy.data.PetContract.PetEntry;
+import com.example.shelterfortommy.data.PetDbHelper;
+
+/**
+ * Allows user to create a new pet or edit an existing one.
+ */
+public class EditorActivity extends AppCompatActivity {
+    /** EditText field to enter the pet's name */
+    private EditText mNameEditText;
+
+    /** EditText field to enter the pet's breed */
+    private EditText mBreedEditText;
+
+    /** EditText field to enter the pet's weight */
+    private EditText mWeightEditText;
+
+    /** EditText field to enter the pet's gender */
+    private Spinner mGenderSpinner;
+    /**
+     * Gender of the pet. The possible values are:
+     * 0 for unknown gender, 1 for male, 2 for female.
+     */
+    private int mGender = 0;// to store the gender
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+        Log.i("###","entered onPostCreate() inside EditorActivity.java");
+
+        //Find all relevant views that will need input from user
+        mNameEditText=(EditText) findViewById(R.id.edit_pet_name);
+        mBreedEditText=(EditText) findViewById(R.id.edit_pet_breed);
+        mWeightEditText=(EditText) findViewById(R.id.edit_pet_weight);
+        mGenderSpinner=(Spinner) findViewById(R.id.spinner_gender);
+        Log.i("###","all the views obtained inside onPostCreate() inside EditorActivity.java");
+        Log.i("###","calling setUpSpinner() inside onPostCreate() in EditorActivity.java");
+
+        setUpSpinner();
+
+    }
+    /**
+     * Setup the dropdown spinner that allows the user to select the gender of the pet.
+     */
+    private void setUpSpinner()
+    {
+        Log.i("###","entered setUpSpinner() in EditorActivity.java");
+
+        //Create adapter for spinner. The list options are from the String array it will use
+        //the spinner wll use the default layout
+        ArrayAdapter genderSpinnerAdapter=ArrayAdapter.createFromResource(this,R.array.array_gender_options,android.R.layout.simple_dropdown_item_1line);
+        //Apply the adapter to the spinner
+        mGenderSpinner.setAdapter(genderSpinnerAdapter);
+        //Set the integer mSelected to the constant values
+        Log.i("###","adapter for the Spinner set setUpSpinner() in EditorActivity.java");
+
+        mGenderSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                Log.i("###","entered onItemSelected() in setUpSpinner() in EditorActivity.java");
+
+                String selection=(String) adapterView.getItemAtPosition(position);
+                if(!TextUtils.isEmpty(selection))
+                {
+                    if(selection.equals(getString(R.string.gender_male)))
+                    mGender= PetEntry.GENDER_MALE;//Form male
+                    else
+                    if(selection.equals(getString(R.string.gender_female)))
+                    mGender=PetEntry.GENDER_FEMALE;//Form female
+                    else
+                    mGender=PetEntry.GENDER_UNKNOWN;//Unknown
+                }//PetContract.PetEntry not required because the class is imported
+                Log.i("###","exiting onItemSelected() in EditorActivity.java");
+            }
+
+            // Because AdapterView is an abstract class, onNothingSelected must be defined
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Log.i("###","entered onNothingSelected() in EditorActivity.java");
+
+                mGender=0;//Unknown
+                Log.i("###","exiting onNothinSelected()");
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.i("###","entered onCreateOptionMenu() in EditorActivity.java");
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_editor,menu);
+        Log.i("###","Inflated the menu editor in " +
+                "onCreateOptionsMenu in EditorActivity.java");
+        return true;
+    }
+    private void insertPet()
+    {
+        Log.i("###","entered the insertPet() method in EditorActivity()");
+        //Read the inputs from the input fields
+        //Note that the Gender value was stored in mGender variable
+        String nameString=mNameEditText.getText().toString().trim();
+        String breedString=mBreedEditText.getText().toString().trim();
+        int  weight=Integer.parseInt(mWeightEditText.getText().toString().trim());
+        //Create a database helper object.
+        PetDbHelper mDbHelper=new PetDbHelper(this);
+        //Get the database in write mode
+        SQLiteDatabase db=mDbHelper.getWritableDatabase();
+        //Create the ContentValues object where column names are the key,
+        //and pet attributes from the editor are the values.
+        ContentValues values=new ContentValues();
+        values.put(PetEntry.COLUMN_PET_NAME,nameString);
+        values.put(PetEntry.COLUMN_PET_BREED,breedString);
+        values.put(PetEntry.COLUMN_PET_GENDER,mGender);
+        values.put(PetEntry.COLUMN_PET_WEIGHT,weight);
+        //Insert a new row for pet in the database, returning the
+        //ID of that new row.
+        //Show the toast accordingly.
+        long newRowID=db.insert(PetEntry.TABLE_NAME,null,values);
+        if(newRowID==-1)
+        {
+            //If the row ID is -1, then there was an error with insertion.
+            Toast.makeText(this,"Error while saving the pet data!",Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            Toast.makeText(this,"Pet saved with row ID: "+newRowID,Toast.LENGTH_LONG).show();
+
+        }
+        Log.i("###","Exiting the insertPet() method of EditorActivity class");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //User clicked on menu option in the app bar overflow menu
+        Log.i("###","entered onOptionsItemSelected()" +
+                " in EditorActivity.java");
+switch(item.getItemId())
+{
+
+    //Respond to a click on the "Save" menu option.
+    case R.id.action_save:
+        //When Save icon (Tick) is clicked, display a toast message
+        //and return back to the main catalog screen.
+        insertPet();
+        //to go back to the catalog screen, call the finish() method.
+        finish();
+        Log.i("###","called the finish() method in EditorActivity()");
+
+        //Display a toast message whether the operating was successful or not.
+        return true;
+    //Respond to a click on the "Delete" menu option.
+    case R.id.action_delete:
+        //Do nothing for now
+        return true;
+    // Respond to a click on the "Up" arrow button in the app bar
+    case android.R.id.home:
+        // Navigate back to parent activity (CatalogActivity)
+        NavUtils.navigateUpFromSameTask(this);
+        return true;
+}
+    Log.i("###","Returning from onOptionsItemSelected() in EditorActivity.java");
+        return super.onOptionsItemSelected(item);
+    }
+}
